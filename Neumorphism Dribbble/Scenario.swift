45226bd5631b7cfcc6b828e469de75c80408f766
Elaidzha1940 <eli080293@gmail.com>
2023-06-09 17:24:36 +0300

    addede a New File / Scenario

--- Neumorphism Dribbble/Scenario.swift
+++ Neumorphism Dribbble/Scenario.swift
@@ -16,8 +16,20 @@ struct Scenario: View {
         
         HStack {
             
-            Text("Turn on on the scenario")
+            Text("Turn on on the scenario of presence")
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(height: 100)
+                Image(systemName: "arrow.right")
+                
+            }
         }
+        .frame(maxWidth: .infinity)
+        .frame(height: 120)
+        .background(Color.gray.opacity(0.5))
+        .cornerRadius(30)
+        .padding(.horizontal, 20)
     }
 }
 
