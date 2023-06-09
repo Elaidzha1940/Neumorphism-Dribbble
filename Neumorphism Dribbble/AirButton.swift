2d6c36874e594164899a2ee6328c338cf1208f67
Elaidzha1940 <eli080293@gmail.com>
2023-06-09 16:43:31 +0300

    addede a New File / AirButton

--- Neumorphism Dribbble/AirButton.swift
+++ Neumorphism Dribbble/AirButton.swift
@@ -25,14 +25,15 @@ struct AirButton: View {
                 
                 Circle()
                     .frame(width: 90, height: 90)
-                    .foregroundColor(.white)
+                    .foregroundColor(.gray.opacity(0.5))
                 
                 Circle()
-                    .frame(width: 90, height: 90)
-                    .foregroundColor(.white)
-                    .shadow(color: .white, radius: 10, x: 15, y: 15)
+                    .frame(width: 60, height: 60)
+                    .foregroundColor(.gray.opacity(0.4))
+                    .shadow(color: .gray.opacity(0.6), radius: 10, x: 15, y: 15)
                     .shadow(color: .white, radius: 5, x: 15, y: 15)
                     .shadow(color: .white, radius: 8, x: -5, y: -5)
+                    .shadow(color: .white, radius: 5, x: -5, y: -5)
                 
                 Circle()
                     .strokeBorder(
@@ -42,9 +43,13 @@ struct AirButton: View {
                         )
                     )
                     .frame(width: 50, height: 50)
-                    .foregroundColor(.white)
+                    .foregroundColor(.white.opacity(0.6))
             }
+            .frame(width: 90, height: 90)
         }
+        .frame(width: 300, height: 160)
+        .background(Color.white.opacity(0.5))
+
     }
 }
 
