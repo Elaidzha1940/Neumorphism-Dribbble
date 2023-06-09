52fe3ed2aceda673befbe3a73a7ed469ea5029bb
Elaidzha1940 <eli080293@gmail.com>
2023-06-09 16:15:45 +0300

    addede a New File / AirButton

---
+++ Neumorphism Dribbble/AirButton.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project:  Neumorphism Dribbble
+//  AirButton.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.06.2023
+//
+//  Status
+//
+//  */
+
+import SwiftUI
+
+struct AirButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AirButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AirButton()
+    }
+}
