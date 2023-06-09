e695ab9b3c83583c844c78f420af582a478b333d
Elaidzha1940 <eli080293@gmail.com>
2023-06-09 16:24:07 +0300

    addede a New File / AirButton

--- Neumorphism Dribbble/AirButton.swift
+++ Neumorphism Dribbble/AirButton.swift
@@ -13,7 +13,38 @@ import SwiftUI
 
 struct AirButton: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack {
+            VStack(alignment: .leading) {
+                
+                Text("AirConditioning")
+                Text("Connected")
+            }
+            
+            ZStack {
+                
+                Circle()
+                    .frame(width: 90, height: 90)
+                    .foregroundColor(.white)
+                
+                Circle()
+                    .frame(width: 90, height: 90)
+                    .foregroundColor(.white)
+                    .shadow(color: .white, radius: 10, x: 15, y: 15)
+                    .shadow(color: .white, radius: 5, x: 15, y: 15)
+                    .shadow(color: .white, radius: 8, x: -5, y: -5)
+                
+                Circle()
+                    .strokeBorder(
+                        style: StrokeStyle(
+                            lineWidth: 2,
+                        dash: [5]
+                        )
+                    )
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.white)
+            }
+        }
     }
 }
 
