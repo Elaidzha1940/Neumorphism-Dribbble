23c91c443001aa3cc827926a982c73bc90a26766
Elaidzha1940 <eli080293@gmail.com>
2023-06-09 17:15:30 +0300

    addede a New File / v

---
+++ Neumorphism Dribbble/Scenario.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: Neumorphism Dribbble
+//  File: Scenario.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.06.2023
+//
+//  Status:
+//
+//  */
+
+import SwiftUI
+
+struct Scenario: View {
+    var body: some View {
+        
+        HStack {
+            
+            Text("Turn on on the scenario")
+        }
+    }
+}
+
+struct Scenario_Previews: PreviewProvider {
+    static var previews: some View {
+        Scenario()
+    }
+}
